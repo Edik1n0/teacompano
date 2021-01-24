e6273c96db833dae9647a72f0e7052ec640ea934
emunera <edisonupb2013@gmail.com>
2021-01-24 11:10:40 -0500

    push

--- src/routes/control.js
+++ src/routes/control.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const webpush = require('web-push');
+require('dotenv').config();
 
 const pool = require('../db');
-
-
 
 module.exports = router;
