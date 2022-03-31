903269422cb6d7b2359d496180c3e01206f6cb52
k1n0trz <kinotrance@gmail.com>
2022-03-31 12:52:16 -0500

    ultimos cambios

--- src/keys.js
+++ src/keys.js
@@ -16,6 +16,15 @@
 //     }
 // };
 
+// module.exports = {
+//     database: {
+//         host: 'ec2-3-86-117-9.compute-1.amazonaws.com',
+//         user: '75o6o7t5qfsmu',
+//         password: 'pdb0ff401966f34bab34be1ba2327080cd653ca39e334247a1456b7564ae86a2a',
+//         database: 'desq37k94ie0gg'
+//     }
+// };
+
 module.exports = {
     database: {
         host: 'us-cdbr-east-03.cleardb.com',
