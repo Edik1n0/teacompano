73599652af6a543194e891397edbbbd1fda5372b
Edik1n0 <35901018+Edik1n0@users.noreply.github.com>
2023-08-17 14:45:15 -0500

    ajustes varios

--- web/admin.py
+++ web/admin.py
@@ -9,7 +9,6 @@ from django.utils.text import slugify
 from django.http import HttpResponse
 from django.urls import reverse
 from django.utils.html import format_html
-from weasyprint import HTML
 
 from .models import Kardex, Asesor, Formulario, Video, Pagina
 
@@ -24,17 +23,71 @@ class KardexAdmin(admin.ModelAdmin):
     generate_pdf_link.short_description = "PDF"
 
     def generate_pdf_action(self, request, queryset):
+        styles = getSampleStyleSheet()
         pdf_buffer = BytesIO()
-        html_content = self.get_html_content(queryset)
-        
-        # Use WeasyPrint to generate the PDF from HTML
-        HTML(string=html_content).write_pdf(pdf_buffer)
 
-        # Set response headers
+        # Crear un documento PDF
+        doc = SimpleDocTemplate(pdf_buffer, pagesize=landscape(letter))
+
+        content = []
+
+        for kardex in queryset:
+            # Crear una lista para el contenido de la página
+            page_content = []
+
+            data = [
+                ['Nombre del paciente', kardex.paciente],
+                ['Diagnóstico', kardex.diagnosis],
+                ['Dieta', kardex.dieta],
+                ['Edad', kardex.edad],
+                ['Peso', kardex.peso],
+                ['Tratamiento', Paragraph(kardex.tratamiento, styles["Normal"])],
+                ['Plan', Paragraph(kardex.plan, styles["Normal"])],
+                ['Fecha de creación', kardex.fecha_kardex.strftime('%Y-%m-%d %H:%M:%S')],
+            ]
+
+            # Definir los estilos para la tabla
+            table_style = TableStyle([
+                ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+                ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+                ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+                ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+                ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+                ('GRID', (0, 0), (-1, -1), 1, colors.black),
+            ])
+
+            # Crear la tabla con los datos y aplicar los estilos
+            table = Table(data)
+            table.setStyle(table_style)
+            page_content.append(table)
+
+            # Agregar imagen de fondo (ajustar la ruta)
+            background_image = 'web/static/img/bg/hoja-horizontal.jpg'  # Ruta correcta
+
+            # Debug: Verificar si la imagen se carga correctamente
+            print("Ruta de la imagen:", background_image)
+
+            # Crear un PageTemplate con la imagen de fondo
+            img = Image(background_image, width=doc.width, height=doc.height)
+            img_frame = Frame(0, 0, doc.width, doc.height)
+            img_frame.addFromList([img], doc)
+            template = PageTemplate(id='background', frames=[img_frame])
+            doc.addPageTemplates([template])
+
+            # Debug: Verificar si el PageTemplate se crea correctamente
+            print("PageTemplate creado con la imagen de fondo:", template)
+
+            # Agregar contenido de la página al documento
+            content.extend(page_content)
+
+        # Generar un nombre de archivo usando el nombre del paciente y la fecha de creación
+        file_name = f"{slugify(kardex.paciente)}-{kardex.fecha_kardex.strftime('%d%m%y')}.pdf"
         response = HttpResponse(content_type='application/pdf')
-        response['Content-Disposition'] = 'attachment; filename="kardex.pdf"'
+        response['Content-Disposition'] = f'attachment; filename="{file_name}"'
 
-        # Write PDF buffer data to response
+        doc.build(content)
+
         pdf_buffer.seek(0)
         response.write(pdf_buffer.read())
         pdf_buffer.close()
@@ -42,37 +95,6 @@ class KardexAdmin(admin.ModelAdmin):
         return response
 
     generate_pdf_action.short_description = "Generar PDF de Kardex"
-
-    def get_html_content(self, queryset):
-        html_content = """
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <style>
-                /* Define your CSS styles here */
-                body {
-                    background-image: url('/web/static/img/bg/hoja-horizontal.jpg');
-                    background-size: cover;
-                    /* Other styles */
-                }
-                table {
-                    /* Table styles */
-                }
-                /* Add more styles as needed */
-            </style>
-        </head>
-        <body>
-            <div class="container kardex">
-                <h1 style="text-align: center;">Kardex</h1>
-                <table style="width: 100%; border-collapse: collapse; margin-top: 20px;">
-                    <!-- Table rows and data -->
-                </table>
-            </div>
-        </body>
-        </html>
-        """
-
-        return html_content
 
 admin.site.register(Kardex, KardexAdmin)
 
