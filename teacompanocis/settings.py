22b7a3afffe774b3e004b00e1316f0419e0f8577
Edik1n0 <35901018+Edik1n0@users.noreply.github.com>
2023-09-26 11:40:16 -0500

    ajustes json

--- teacompanocis/settings.py
+++ teacompanocis/settings.py
@@ -147,7 +147,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'web/static')
 STATIC_URL = "/static/"  # Deja esto como la URL para servir archivos estáticos
 
 MEDIA_URL = '/media/'
