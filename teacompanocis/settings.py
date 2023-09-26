499d45f453c8bd410fa0230a21a42aa4a1063761
Edik1n0 <35901018+Edik1n0@users.noreply.github.com>
2023-09-26 12:21:49 -0500

    config

--- teacompanocis/settings.py
+++ teacompanocis/settings.py
@@ -148,6 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'web/static')
 STATIC_URL = "/static/"  # Deja esto como la URL para servir archivos estáticos
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
